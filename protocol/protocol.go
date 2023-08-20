a6e8ddf0368923f207596da95f1a2e5a6cde07d7
Ching Pei Yang <badnam3o.0@gmail.com>
2023-08-21 01:45:33 +0200

    initial commit

---
+++ protocol/protocol.go
@@ -0,0 +1,31 @@
+package protocol
+
+// idk if there is proper terminology for this, but "Event" means server to client,
+// "Request" means client to server
+
+type Event struct {
+	Message      EventMessage `json:"message"`
+	StartFirst   bool         `json:"startFirst,omitempty"`
+	OpponentMove [2]string    `json:"opponentMove,omitempty"`
+	Winner       string       `json:"winner,omitempty"`
+}
+
+type Request struct {
+	Request RequestType `json:"request"`
+	Move    [2]string   `json:"move,omitempty"`
+}
+
+type EventMessage string
+
+const (
+	GameStart   EventMessage = "gameStart"
+	PlayerTurn  EventMessage = "playerTurn"
+	IllegalMove EventMessage = "illegalMove"
+	GameEnded   EventMessage = "gameEnded"
+)
+
+type RequestType string
+
+const (
+	Move RequestType = "move"
+)
