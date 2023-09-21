ba7c8a61dafabf2624f891a9d85d5ed8054ee7f5
Ching Pei Yang <badnam3o.0@gmail.com>
2023-09-21 19:44:36 +0200

    dev: draw board on screen

--- pages/chess.js
+++ pages/chess.js
@@ -1,5 +1,9 @@
-"use strict"
+"use strict";
 
+function dbg(x) {
+  console.log(x);
+  return x;
+}
 
 class AppState {
   constructor() {
@@ -12,49 +16,61 @@ class AppState {
   start_socket() {
     let socket = new WebSocket("ws://localhost:9990/game");
 
-    socket.onopen = function(_e) { };
+    socket.onopen = function (_e) {
+      document.getElementById("notification").innerHTML =
+        "Looking for opponent...";
+    };
 
-    socket.onmessage = function(event) {
-      switch (event.data.message) {
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+      switch (data.message) {
         case "gameStart": {
-          this.startGame(event.data.startFirst ? "white" : "black");
+          this.startGame(data.startFirst ? "white" : "black");
+          document.getElementById("notification").innerHTML = "";
           break;
         }
         case "playerTurn": {
-          const [from, to] = event.data.opponentmove;
-          this.move(coordFromNotation(from), coordFromNotation(to));
+          const [from, to] = data.opponentMove;
+          this.move(this.coordFromNotation(from), this.coordFromNotation(to));
           break;
         }
 
         case "moveAccepted":
+          this.selected_cell = null;
+          this.selected_target = null;
           this.move(this.selected_cell, this.selected_target);
           break;
 
         case "illegalMove": {
+          this.selected_cell = null;
+          this.selected_target = null;
           const notification = document.getElementById("notification");
-          const notice = document.createElement("p", { "class": "error" });
-          notice.innerHTML = "Illegal Move"
-          notification.replaceChild(notification.firstChild, notice);
+          const notice = document.createElement("p", { class: "error" });
+          notice.innerHTML = "Illegal Move";
+          notification.replaceChildren(notice);
           break;
         }
 
         default:
-          console.log("received unknown websocket data ", event.data)
+          console.log("received unknown websocket data ", data);
           break;
       }
     };
 
-    socket.onclose = function(event) {
+    socket.onclose = function (event) {
       if (event.wasClean) {
-        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        alert(
+          `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
+        );
       } else {
         // e.g. server process killed or network down
         // event.code is usually 1006 in this case
-        alert('[close] Connection died');
+        alert("[close] Connection died");
       }
     };
 
-    socket.onerror = function(_error) {
+    socket.onerror = function (_error) {
       alert(`[error]`);
     };
     return socket;
@@ -70,97 +86,125 @@ class AppState {
 
     const row_0 = document.getElementById("row-0");
     const row_7 = document.getElementById("row-7");
-    let home_row = ["castle", "knight", "bishop", "queen", "king", "bishop", "knight", "castle"];
+    let home_row = [
+      "rook",
+      "knight",
+      "bishop",
+      "queen",
+      "king",
+      "bishop",
+      "knight",
+      "rook",
+    ];
     if (color == "black") {
       home_row = home_row.reverse();
     }
 
     for (let i in home_row) {
       const type = home_row[i];
-      row_0.childNodes[i].replaceChildren(this.newUnit(opp_color, type));
-      row_7.childNodes[i].replaceChildren(this.newUnit(color, type));
+      row_0.children[i].appendChild(AppState.newUnit(opp_color, type));
+      row_7.children[i].appendChild(AppState.newUnit(color, type));
     }
 
-    for (const cell of document.getElementById("row-1")) {
-      cell.replaceChildren(this.newUnit(opp_color, "pawn"));
+    for (const cell of document.getElementById("row-1").children) {
+      cell.appendChild(AppState.newUnit(opp_color, "pawn"));
     }
 
-    for (let cell of document.getElementById("row-6")) {
-      cell.replaceChildren(this.newUnit(color, "pawn"));
+    for (const cell of document.getElementById("row-6").children) {
+      cell.appendChild(AppState.newUnit(color, "pawn"));
+    }
+
+    const board = document.getElementById("board-body");
+    for (let i = 0; i < board.children.length; i++) {
+      const row = board.children[i];
+      for (let j = 0; j < row.children.length; j++) {
+        const cell = row.children[j];
+        cell.onclick = () => this.select_cell(j, i);
+      }
     }
   }
 
   static newUnit(color, type) {
-    const unit = document.createElement("span");
+    const unit = document.createElement("div");
     unit.classList.add(color, type);
     return unit;
   }
 
   move(from, to) {
     const cfrom = document.getElementById(`cell-${from.x}-${from.y}`);
+    console.assert(cfrom);
     const cto = document.getElementById(`cell-${to.x}-${to.y}`);
+    console.assert(cto);
     const unit = cfrom.removeChild(cfrom.firstChild);
-    cto.replaceChild(cto.firstChild, unit);
-
+    cto.replaceChildren(unit);
   }
 
   select_cell(x, y) {
+    console.log("selecting ", x, y);
     if (this.selected_cell == null) {
       const selection = document.getElementById(`cell-${x}-${y}`);
-      if (selection.firstChild == null || !selection.firstChild.classList.contains(this.color)) {
+      console.assert(selection);
+      if (
+        selection.firstChild == null ||
+        !selection.firstChild.classList.contains(this.color)
+      ) {
         return;
       }
       this.selected_cell = { x: x, y: y };
     } else {
       this.selected_target = { x: x, y: y };
-      this.socket.send({
-        "request": "move", "move": [
-          coordToNotation(this.selected_cell.x, this.selected_cell.y),
-          coordToNotation(x, y)
-        ]
-      })
+      this.socket.send(
+        JSON.stringify({
+          request: "move",
+          move: [
+            dbg(
+              this.coordToNotation(this.selected_cell.x, this.selected_cell.y),
+            ),
+            dbg(this.coordToNotation(x, y)),
+          ],
+        }),
+      );
     }
   }
-}
 
-function coordToNotation(x, y) {
-  if (this.color == 'white') {
-    return `${String.fromCodePoint('a'.codePointAt(0) + x)}${y}`;
-  } else {
-    x = 7 - x;
-    y = 7 - y;
-    return `${String.fromCodePoint('a'.codePointAt(0) + x)}${y}`;
+  coordToNotation(x, y) {
+    if (this.color == "white") {
+      x = 7 - x;
+      y = 7 - y;
+    }
+
+    return `${String.fromCodePoint("a".codePointAt(0) + x)}${y}`;
+  }
+
+  coordFromNotation(s) {
+    let x = s.codePointAt(0) - "a".codePointAt(0);
+    let y = s.codePointAt(1) - "1".codePointAt(0);
+
+    if (this.color == "white") {
+      x = 7 - x;
+      y = 7 - y;
+    }
+
+    return { x: x, y: y };
   }
 }
 
-function coordFromNotation(s) {
-  let x = s.codePointAt(0) - 'a'.codePointAt(0);
-  let y = s.codePointAt(1) - '1'.codePointAt(0);
-
-  if (this.color == 'white') {
-    x = 7 - x;
-    y = 7 - y;
-  }
-
-  return { x: x, y: y };
-}
-
-window.onload = function() {
-  const tbody = document.getElementById('board-body');
+window.onload = function () {
+  const tbody = document.getElementById("board-body");
 
   for (let i = 0; i < 8; i++) {
-    const tr = document.createElement('tr');
+    const tr = document.createElement("tr");
     tr.id = `row-${i}`;
 
     for (let j = 0; j < 8; j++) {
-      const td = document.createElement('td');
+      const td = document.createElement("td");
       td.id = `cell-${j}-${i}`;
-      td.classList.add((i + j) % 2 == 0 ? "tile-dark" : "tile-light")
-      tr.appendChild(td)
+      td.classList.add((i + j) % 2 == 0 ? "tile-dark" : "tile-light");
+      tr.appendChild(td);
     }
     tbody.appendChild(tr);
   }
 
   new AppState();
   // const socket = start_socket();
-}
+};
